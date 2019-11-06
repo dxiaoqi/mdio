66420dee82198c080dad3aa3773f55db3cd30546
丁维涛 <941645@nd.com>
2019-11-06 09:50:35 +0800

    增添webpack & react ts 依赖

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+module.exports = {
+  entry: {
+    
+  },
+  output: {
+      filename: 'budle.js',
+      path: path.resolve('dist')
+  },
+  modules: {},
+  mode: 'development'
+}
