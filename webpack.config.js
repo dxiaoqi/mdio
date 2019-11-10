a8cbed2f82f1dd7d1ebebaa36923a2708a3f60ce
dxiaoqi <1035038018@qq.com>
2019-11-10 23:28:28 +0800

    配置webpack与ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,35 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-  entry: {
-    
-  },
-  output: {
-      filename: 'budle.js',
-      path: path.resolve('dist')
-  },
-  module: {
-    rules: [
-      {
-        test : /\.js$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader'
-      }
+    entry: {
+        app: ['./view/App.ts'],
+        vendor: ['react', 'react-dom']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].bundle.js'
+    },
+    devtool: "source-map",
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".jsx", ".json"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader"
+            },{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {loader: 'babel-loader'}
+            }
+        ]
+    },
+
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./view/index.html"
+        })
     ]
-  },
-  mode: 'development'
-}
+};
